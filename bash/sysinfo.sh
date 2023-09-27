236850af1b9f7d14a2b50f6108945eb303260462
Divya-Patel1403 <divyakpatel0104@gmail.com>
2023-09-27 19:15:58 -0400

    sysinfo (lab 2)

---
+++ bash/sysinfo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#system information 
+$ ./sysinfo.sh 
+FQDN: divyapatel@pc200516980
+#basic information gained by hostnamectl command
+Host Information:
+   Static hostname: pc200516980
+         Icon name: computer-vm
+           Chassis: vm
+        Machine ID: 05ced96f071141be82444ed77522d12a
+           Boot ID: 07dad6177d3c49cc9d0ba34114565a99
+    Virtualization: vmware
+  Operating System: Ubuntu 22.04.3 LTS
+            Kernel: Linux 6.2.0-33-generic
+      Architecture: x86-64
+   Hardware Vendor: VMware, Inc.
+    Hardware Model: VMware Virtual Platform
+#working ip address info.
+IP Addresses:
+192.168.118.128 fe80::4674:8320:7b0e:ba05/64
+#storage used by the root directory in human readable format
+Root Filesystem Status:
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/sda3        39G   15G   22G  41% /
