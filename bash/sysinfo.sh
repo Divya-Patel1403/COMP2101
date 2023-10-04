25a1c2531c3b73e3446001c409b2b81f1641a17b
Divya-Patel1403 <divyakpatel0104@gmail.com>
2023-10-04 01:33:16 -0400

    Lab 1-sysinfo..sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
-#system information 
-$ ./sysinfo.sh 
-FQDN: divyapatel@pc200516980
-#basic information gained by hostnamectl command
-Host Information:
-   Static hostname: pc200516980
-         Icon name: computer-vm
-           Chassis: vm
-        Machine ID: 05ced96f071141be82444ed77522d12a
-           Boot ID: 07dad6177d3c49cc9d0ba34114565a99
-    Virtualization: vmware
-  Operating System: Ubuntu 22.04.3 LTS
-            Kernel: Linux 6.2.0-33-generic
-      Architecture: x86-64
-   Hardware Vendor: VMware, Inc.
-    Hardware Model: VMware Virtual Platform
-#working ip address info.
-IP Addresses:
-192.168.118.128 fe80::4674:8320:7b0e:ba05/64
-#storage used by the root directory in human readable format
-Root Filesystem Status:
-Filesystem      Size  Used Avail Use% Mounted on
-/dev/sda3        39G   15G   22G  41% /
+
+# Function to display a horizontal line
+print_line() {
+    echo "----------------------------------------"
+}
+
+# Display the fully-qualified domain name (FQDN)
+fqdn=$(hostname)
+echo "FQDN: $fqdn"
+print_line
+
+# Display host information using hostnamectl
+echo "Host Information:"
+hostnamectl | grep "Static hostname"
+hostnamectl | grep "Icon name"
+hostnamectl | grep "Chassis"
+hostnamectl | grep "Machine ID"
+hostnamectl | grep "Boot ID"
+print_line
+
+# Display operating system information using hostnamectl
+echo "Operating System:"
+hostnamectl | grep "Operating System"
+hostnamectl | grep "Kernel"
+hostnamectl | grep "Architecture"
+print_line
+
+# Display IP addresses that are not on the 127 network
+echo "IP Addresses:"
+ip addr show | grep "inet " | awk '{print $2}' | grep -v "^127"
+print_line
+
+# Display root filesystem status using df
+echo "Root Filesystem Status:"
+df -h / | tail -n 1
+
